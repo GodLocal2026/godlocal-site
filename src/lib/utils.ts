7171dbdf7bb7c7e7899c96c3c15f15129fa3a57d
GodLocal2026 <kitbtc100@gmail.com>
2026-02-27 19:06:53 +0000

    feat: add src/lib/utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,5 @@
-import { type ClassValue, clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) { return twMerge(clsx(inputs)); }
-export const GITHUB_URL = 'https://github.com/GODLOCAL/godlocal';
-export const TWITTER_URL = 'https://x.com/GodLocal';
-export const TWITTER_RU_URL = 'https://x.com/GodLocalRU';
-export const TWITTER_DEV_URL = 'https://x.com/GodLocalDev';
+export const GITHUB_URL = "https://github.com/GodLocal2026";
+export const TWITTER_URL = "https://x.com/kitbtc";
